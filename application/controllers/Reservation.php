4966bd32126aca3b0ad5b5723fb195e3c8a6a133
Prabuddha <Prabuddha.proart@gmail.com>
2019-02-23 10:01:11 +0530

    mail fix in reservation class

--- application/controllers/Reservation.php
+++ application/controllers/Reservation.php
@@ -42,13 +42,15 @@ class Reservation extends CI_Controller{
                 if($this->session->has_userdata('name')) {
                     //Check the session wheather it is same
                     if($this->session->userdata('phoneNo') == $databaseData['phoneNo'] && $this->session->userdata('date') == $databaseData['date'] && $this->session->userdata('time') == $databaseData['time'] && $this->session->userdata('name') == $databaseData['name'] && $this->session->userdata('email') == $databaseData['email'] && $this->session->userdata('people') == $databaseData['people'] ){
-                        $message = "You have entered the same value again";
+                        $message = "You have entered the same values again";
                     } else {
                         //send to database
                         $result =  $this->reservation_model->bookTable($databaseData);
                         if($result == false){
                             $message =  "Sorry! All the Tables booked at the time";
                         } else {
+                            $subject = "Grand Oceanpark - Table Bookings";
+                            $title = "Table Bookings";
                             $this->sendMail($databaseData,$result);
                             $message =  "Booking Successful";
                     }
@@ -59,6 +61,8 @@ class Reservation extends CI_Controller{
                 if($result == false){
                         $message =  "Sorry! All the Tables booked at the time";
                 } else {
+                    $subject = "Grand Oceanpark - Table Bookings";
+                    $title = "Table Bookings";
                     $this->sendMail($databaseData,$result);
                     $message =  "Booking Successful";
                 } 
@@ -113,14 +117,16 @@ class Reservation extends CI_Controller{
                 if($this->session->has_userdata('name')) {
                     //Check the session wheather it is same
                     if($this->session->userdata('phone') == $databaseData['phoneNo'] && $this->session->userdata('date') == $databaseData['date'] && $this->session->userdata('time') == $databaseData['time'] && $this->session->userdata('name') == $databaseData['name'] && $this->session->userdata('email') == $databaseData['email'] && $this->session->userdata('hall_type') == $databaseData['hall_type'] ){
-                        $message = "You have entered the same value again";
+                        $message = "You have entered the same values again";
                     } else {
                         //send to database
                         $result =  $this->reservation_model->bookHall($databaseData);
                         if($result == false){
                             $message =  "Sorry! All the Tables booked at the time";
                         } else {
-                            $this->sendMail($databaseData,$result);
+                            $subject = "Grand Oceanpark - Hall Bookings";
+                            $title = "Hall Bookings";
+                            $this->sendMail($databaseData,$result,$subject,$title);
                             $message =  "Booking Successful";
                     }
                 }
@@ -130,12 +136,12 @@ class Reservation extends CI_Controller{
                 if($result == false){
                         $message =  "Sorry! All the Tables booked at the time";
                 } else {
+                    $subject = "Grand Oceanpark - Hall Bookings";
+                    $title = "Hall Bookings";
                     $this->sendMail($databaseData,$result);
                     $message =  "Booking Successful";
                 } 
-            
             }
-
             echo $message;
             }
            
@@ -143,20 +149,23 @@ class Reservation extends CI_Controller{
     
     }
 
-    public function sendMail($data,$id){
+    public function sendMail($data,$id,$subject,$title){
         $this->load->helper('email_helper');
 
         if(array_key_exists('people', $data)){
             $dataHolder = $data['people'];
+            $bookingNumberLetter = 'T';
+            
         } else {
             $dataHolder = $data['hall_type'];
+            $bookingNumberLetter = 'H';
+            
         }
 
         $emailAddress = 'it@sunwayholidays.lk';
         $name = 'Prabuddha';
-        $subject = 'Grand Oceanpark Table Bookings';
-        $content = '<h1>Grand Oceanpark Restaurent - Table Bookings </h1>';
-        $content = '<h2>Booking Number - GOPTB0'. $id . '</h2>';
+        $content = '<h1>Grand Oceanpark Restaurent - '. $title .'</h1>';
+        $content .= '<h2>Booking Number - GOP'.$bookingNumberLetter.'B0'. $id . '</h2>';
         $content .= '<h3>This mail is generated with the information of the booking iteslf</h3><br>';
         $content .= '<p>Name - '.$data['name'] . '</p>';
         $content .= '<p>Email - '.$data['email'] . '</p>';
